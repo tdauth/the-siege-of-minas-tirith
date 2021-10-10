4e34088efa323aa7567b5ed7aa77c50077402cb5
tdauth <barade.barade@web.de>
2021-10-10 14:11:29 +0200

    Improvements
    - Quest log notes that houses drop random items.
    - Change limit of Battle Trolls from 5 to 3 and make them stronger and cost more food.
    - Limit food to 40 per player in 4 vs 4 and to 160 in 1 vs 1.
    - Decrease movement speed of Siege Towers.
    - Players are not neutral anymore during the preparation since you could see the others chat messages.
    - Add Siege Tower 7 to second wall.

--- updatemap.bat
+++ updatemap.bat
@@ -1,2 +1,2 @@
-copy C:\Users\Tamino\Documents\Projekte\the-siege-of-minas-tirith\TheSiegeOfMinasTirith0.6.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+copy C:\Users\Tamino\Documents\Projekte\the-siege-of-minas-tirith\TheSiegeOfMinasTirith0.7.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 pause
